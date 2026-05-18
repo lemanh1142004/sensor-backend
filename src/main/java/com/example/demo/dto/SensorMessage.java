7bb6242cd664ef9a51fa09b9187f6b9211df5f76
lemanh1142004 <bui452893@gmail.com>
2026-05-18 17:48:46 +0700

    Fix sensor realtime update and history save logic

--- src/main/java/com/example/demo/dto/SensorMessage.java
+++ src/main/java/com/example/demo/dto/SensorMessage.java
@@ -1,17 +1,36 @@
 package com.example.demo.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SensorMessage {
+
     private String deviceId;
+
+    @JsonProperty("temp")
     private Double temperature;
+
+    @JsonProperty("hum")
     private Double humidity;
+
     private Double gas;
     private Double smoke;
+
+    @JsonProperty("isSafe")
     private Boolean isSafe;
+
+    @JsonProperty("manualMode")
     private Boolean manualMode;
+
     private Boolean fan;
     private Boolean buzzer;
+
+    @JsonProperty("ledRed")
     private Boolean ledRed;
+
     private Integer activeAlerts;
 }
